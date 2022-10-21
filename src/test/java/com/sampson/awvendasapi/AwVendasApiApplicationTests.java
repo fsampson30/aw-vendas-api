a82f71b4ae8c7e9fd3d07dfcb142b0a78df3dd49
fsampson30 <fsampson30@gmail.com>
2022-10-21 07:34:19 -0300

    Initial commit

---
+++ src/test/java/com/sampson/awvendasapi/AwVendasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampson.awvendasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwVendasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
